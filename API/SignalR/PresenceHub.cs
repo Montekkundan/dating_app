a129be296137e002777c4fde0077f73c16eac29c
Montek <87750128+Montekkundan@users.noreply.github.com>
2023-03-28 16:15:18 -0700

    added signalr

--- API/SignalR/PresenceHub.cs
+++ API/SignalR/PresenceHub.cs
@@ -1,12 +1,19 @@
+using API.Extensions;
 using Microsoft.AspNetCore.SignalR;
 
 namespace API.SignalR
 {
     public class PresenceHub : Hub
     {
-        public override Task OnConnectedAsync()
+        public override async Task OnConnectedAsync()
         {
-            return base.OnConnectedAsync();
+            await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
+        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+             await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+
+             await base.OnDisconnectedAsync(exception);
         }
     }
 }
